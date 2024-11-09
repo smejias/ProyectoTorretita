8979816ac7311e2b050dbad88178bb20d989cee4
smejias <santiagojmejias@gmail.com>
2024-11-09 15:37:51 -0300

    sonidos - luquitas

--- Scripts/collectible.gd
+++ Scripts/collectible.gd
@@ -1,7 +1,10 @@
 extends Area2D
 
+@onready var pickup = $Money_sound
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		GlobalVariables.score += 150
+		pickup.play()
+		await pickup.finished
 		queue_free()
